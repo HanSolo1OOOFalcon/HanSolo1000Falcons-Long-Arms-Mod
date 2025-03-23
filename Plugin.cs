775981bec5791722d7a588feaf30d51ce3a57e73
HanSolo1000Falcon <202703912+HanSolo1OOOFalcon@users.noreply.github.com>
2025-03-23 21:55:17 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -3,32 +3,31 @@ using BepInEx;
 using UnityEngine;
 using Utilla;
 
-namespace LongArms
+namespace HanSolo1000FalconsLongArmsMod
 {
 	/// <summary>
 	/// This is your mod's main class.
 	/// </summary>
 
 	/* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
-    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
+	public static bool ciEnabled = true;
+		
 		void OnEnable()
 		{
-			/* Set up your mod here */
-			/* Code here runs at the start and whenever your mod is enabled*/
+			ciEnabled = true;
 
 			HarmonyPatches.ApplyHarmonyPatches();
 		}
 
 		void OnDisable()
 		{
-			/* Undo mod setup here */
-			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
-			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+			ciEnabled = false;
 
-			HarmonyPatches.RemoveHarmonyPatches();
+            		HarmonyPatches.RemoveHarmonyPatches();
 		}
 
 		void OnGameInitialized(object sender, EventArgs e)
@@ -51,9 +50,16 @@ namespace LongArms
         {
             if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
             {
-                if (GorillaLocomotion.GTPlayer.Instance != null)
-                {
-                    GorillaLocomotion.GTPlayer.Instance.transform.localScale = new Vector3(playerScale, playerScale, playerScale);
+                if (ciEnabled)
+				{
+                    if (GorillaLocomotion.GTPlayer.Instance != null)
+                    {
+                        GorillaLocomotion.GTPlayer.Instance.transform.localScale = new Vector3(playerScale, playerScale, playerScale);
+                    }
+                }
+		else
+		{
+			GorillaLocomotion.GTPlayer.Instance.transform.localScale = new Vector3(1f, 1f, 1f);
                 }
             }
         }
@@ -65,79 +71,88 @@ namespace LongArms
             }
         }
 
-	public static float playerScale = 1f;
-	private static bool yPress = false;
-        private static float lastTime = 0f;
-       	private static bool toggled = true;
-	public static bool osToggled = true;
+public static float playerScale = 1f;
+private static bool yPress = false;
+private static float lastTime = 0f;
+private static bool toggled = true;
+public static bool osToggled = true;
 
-	public static float increment = 0.05f;
-	public static float cooldown = 0.05f;
-	public static float max = 3f;
-	public static float least = 0.5f;
-
-	
- 	void FixedUpdate()
-        {
+public static float increment = 0.025f;
+public static float cooldown = 0.05f;
+public static float max = 3f;
+public static float least = 0.5f;
+void FixedUpdate()
+{
 	bool lT = ControllerInputPoller.instance.leftControllerIndexFloat > 0.5f;
 	bool lG = ControllerInputPoller.instance.leftGrab;
 	bool Y = ControllerInputPoller.instance.leftControllerSecondaryButton;
 	bool X = ControllerInputPoller.instance.leftControllerPrimaryButton;
 
-	if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+	if (NetworkSystem.Instance.InRoom)
 	{
-		if (lT && toggled && !lG && osToggled)
-		{
-			if (lastTime >= cooldown)
-			{
-				lastTime = 0f;
-				playerScale += increment;
-			}
-		}
-
-                if (lG && toggled && !lT && osToggled)
+                if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
                 {
-                    	if (lastTime >= cooldown)
-                    	{
-                        	lastTime = 0f;
-                        	playerScale -= increment;
-                    	}
+                    if (lT && toggled && !lG && osToggled)
+                    {
+                        if (lastTime >= cooldown)
+                        {
+                            lastTime = 0f;
+                            playerScale += increment;
+                        }
+                    }
+
+                    if (lG && toggled && !lT && osToggled)
+                    {
+                        if (lastTime >= cooldown)
+                        {
+                            lastTime = 0f;
+                            playerScale -= increment;
+                        }
+                    }
+
+                    if (Y && osToggled)
+                    {
+                        if (!yPress)
+                        {
+                            yPress = true;
+                            toggled = !toggled;
+                        }
+                    }
+                    else
+                    {
+                        yPress = false;
+                    }
+
+                    if (playerScale > max)
+                    {
+                        playerScale = max;
+                    }
+
+                    if (playerScale < least)
+                    {
+                        playerScale = least;
+                    }
+
+                    if (X && toggled && osToggled)
+                    {
+                        playerScale = 1f;
+                    }
+
+                    if (!ciEnabled)
+                    {
+                        if (playerScale != 1f)
+                        {
+                            playerScale = 1f;
+                        }
+                    }
+
+                    if (GorillaLocomotion.GTPlayer.Instance != null)
+                    {
+                        GorillaLocomotion.GTPlayer.Instance.transform.localScale = new Vector3(playerScale, playerScale, playerScale);
+                    }
+
+                    lastTime += Time.deltaTime;
                 }
-
-		if (Y && osToggled)
-		{
-			if (!yPress)
-			{
-				yPress = true;
-				toggled = !toggled;
-			}
-		}
-		else
-		{
-			yPress = false;
-		}
-
-		if (playerScale > max)
-		{
-			playerScale = max;
-		}
-
-		if (playerScale < least)
-		{
-			playerScale = least;
-		}
-
-		if (X && toggled && osToggled)
-		{
-			playerScale = 1f;
-		}
-				
-		if (GorillaLocomotion.GTPlayer.Instance != null)
-		{
-                    	GorillaLocomotion.GTPlayer.Instance.transform.localScale = new Vector3(playerScale, playerScale, playerScale);
-                }
-
-		lastTime += Time.deltaTime;
             }
 	}
     }
